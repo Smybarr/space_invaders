010523d3d9035c6aa2b7ab313b66551f1b4c2a1b
Steven Ybarra <Smybarr@gmail.com>
2018-09-07 18:25:13 -0500

    Added Spritebase
    Added sprite base class to act as both a child class of DLink and parent class of Sprite class. SpriteBase holds the coordinate data for rendering the sprite graphics. Purpose is to serve the same purpose for sprite boxes to test collision events in a future update.

--- SpaceInvaders/Sprite/SpriteBase.cs
+++ SpaceInvaders/Sprite/SpriteBase.cs
@@ -6,26 +6,13 @@ namespace SpaceInvaders
 {
     public abstract class SpriteBase : DLink
     {
-        // Create a single sprite and all dynamic objects ONCE and ONLY ONCE (OOO- tm)
-        public SpriteBase()
-            : base()
-        {
-            this.x = 0.0f;
-            this.y = 0.0f;
-            this.sx = 1.0f;
-            this.sy = 1.0f;
-            this.angle = 0.0f;
-        }
-
-        abstract public void Update();
-        abstract public void Draw();
-
-        // Data: -------------------------------------------
-
         public float x;
         public float y;
         public float sx;
         public float sy;
         public float angle;
+
+        public abstract void Update();
+        public abstract void Draw();
     }
 }
